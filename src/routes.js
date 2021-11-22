78ea807152ffc68c19568b6c81a5ad7647bf5f1d
Gabriel Dal Bó <gabrielpdalbo@gmail.com>
2021-11-22 05:11:28 -0300

    views of site moved to /site in /views

--- src/routes.js
+++ src/routes.js
@@ -4,17 +4,17 @@ const data = require('./data.json')
 const recipes = require('./controllers/recipes')
 
 routes.get('/', (req, res) => {
-  return res.render('home', { recipes: data.recipes })
+  return res.render('site/home', { recipes: data.recipes })
 })
 routes.get('/about', (req, res) => {
-  return res.render('about')
+  return res.render('site/about')
 })
 routes.get('/recipes', (req, res) => {
-  return res.render('recipes', { recipes: data.recipes })
+  return res.render('site/recipes', { recipes: data.recipes })
 })
 routes.get('/recipes/:id', (req, res) => {
   const { id } = req.params
-  return res.render('info', { recipe: data.recipes[id] })
+  return res.render('site/info', { recipe: data.recipes[id] })
 })
 
 /* ADMIN */
@@ -27,7 +27,6 @@ routes.get('/admin/recipes', recipes.index) // Mostrar a lista de receitas
 routes.get('/admin/recipes/create', recipes.create) // Mostrar formulário de nova receita
 routes.get('/admin/recipes/:id', recipes.show) // Exibir detalhes de uma receita
 routes.get('/admin/recipes/:id/edit', recipes.edit) // Mostrar formulário de edição de receita
-
 routes.post('/admin/recipes', recipes.post) // Cadastrar nova receita
 routes.put('/admin/recipes', recipes.put) // Editar uma receita
 routes.delete('/admin/recipes', recipes.delete) // Deletar uma receita
